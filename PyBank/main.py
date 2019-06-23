8c9dfb9be8a5064c79dcca234ead9697131c74fa
alanacsaposs <alanacsaposs@gmail.com>
2019-06-22 18:07:25 -0700

    Final Uploads for PyBank and Pypoll code and outputs

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,31 +10,53 @@ months = []
 monthly_change = []
 with open(budget_csv, newline="") as csvfile:
 
-    # Split the data on commas
+# Split the data on commas
     budgetfile = csv.reader(csvfile, delimiter=',')
     header = next(budgetfile)
     total = 0
+    month_revenue = 0
 
-    # Read through each row of data after the header
+# Read through each row of data after the header
     for row in budgetfile:
         months.append(row[0])
         total += int(row[1])
-
-    #Calculate monthly change
-    for i in budgetfile:
-    # Take the difference between two months and append to monthly profit change
-        monthly_change.append(total[i+1]-total[i])
         
-    #Find maximum and minimum monthly change
-        max_increase_value = max(monthly_change)
-        max_decrease_value = min(monthly_change)
+# Take the difference between two months and append to monthly profit change
+        monthly_change.append(int(row[1]) - month_revenue)
+        month_revenue = int(row[1])
+        
+#Find maximum and minimum monthly change
+    #Greatest increase in profits
+        max_increase = max(monthly_change)
+        best_index = monthly_change.index(max_increase)
+        best_date = months[best_index]
 
+    #Greatest decrease (lowest increase) in profits 
+        max_decrease = min(monthly_change)
+        worst_index = monthly_change.index(max_decrease)
+        worst_date = months[worst_index]
 
-    #print
+    #Get Average Change
+        change_total = sum(monthly_change) - 867884
+        total_months = len(months)
+        avg_change = round(change_total/total_months, 2)
+
+#print statements
 print("Financial Analysis")
 print("-------------------------")
 print(f"Total Months: {len(months)}")
 print(f"Total: ${(total)}")
+print(f"Average Change: ${avg_change}")
+print(f"Greatest Increase in Profits: {best_date} (${str(max_increase)})")
+print(f"Greatest Decrease in Profits: {worst_date} (${str(max_decrease)})")
 
-    
-    #save as .txt file
+# save to .txt file
+filepath = os.path.join("output_pybank.txt")
+with open(filepath,'w') as text:
+    text.write("Financial Analysis" + "\n")
+    text.write("-------------------------" + "\n")
+    text.write(f"Total Months: {len(months)}" + "\n")
+    text.write(f"Total: ${(total)}" + "\n")
+    text.write(f"Average Change: ${avg_change}" + "\n")
+    text.write(f"Greatest Increase in Profits: {best_date} (${str(max_increase)})" + "\n")
+    text.write(f"Greatest Decrease in Profits: {worst_date} (${str(max_decrease)})" + "\n")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -5,20 +5,21 @@ import csv
 # import csv
 poll_csv = os.path.join('..', 'PyPoll', 'election_data.csv')
 
-#
+# set list variables
 candidates = []
 total_votes = []
 candidate_votes = []
-#
+# read csv
 with open(poll_csv, newline="") as poll_data:
     election_data = csv.reader(poll_data, delimiter=',')
     header = next(election_data)    
 
-# lists for variables
-
+# calculate total votes by appending total_votes by row
     for row in election_data:
         total_votes.append(row[0])
-
+# build lis of all candidates: if candidate exists, add to current index of votes
+#   if not, add candidate and append candidates list to include candidate
+#       and append candidate_votes list to include new vote count
         if row[2] in candidates:
             candidates_index = candidates.index(row[2])
             candidate_votes[candidates_index] += 1
@@ -26,17 +27,19 @@ with open(poll_csv, newline="") as poll_data:
         else:
             candidates.append(row[2])
             candidate_votes.append(1)
-
+# set list for percentages
 vote_percentage = []
+# for each candidate in the candidates list, calculate percentage of votes
 for count in range(len(candidates)):
     percentage = candidate_votes[count]/(len(total_votes))*100
     percentage = round(percentage)
     vote_percentage.append(percentage)
-
+# set winner as max votes, set winning_candidate to the candidate sharing that index
 winner = max(candidate_votes)
 index = candidate_votes.index(winner)
 winning_candidate = candidates[index]
 
+# print statements
 print("Election Results")
 print("-------------------------")
 print (f"Total Votes: {len(total_votes)}")
@@ -46,3 +49,16 @@ for i in range(len(candidates)):
 print("-------------------------")
 print (f"Winner: {winning_candidate}")
 print("-------------------------")
+
+# save to .txt file
+filepath = os.path.join("output_pypoll.txt")
+with open(filepath,'w') as text:
+    text.write("Election Results" + "\n")
+    text.write("-------------------------" + "\n")
+    text.write(f"Total Votes: {len(total_votes)}" + "\n")
+    text.write("-------------------------" + "\n")
+    for i in range(len(candidates)):
+        text.write(f"{candidates[i]}: {str(vote_percentage[i])}% ({str(candidate_votes[i])})" + "\n")
+    text.write("-------------------------" + "\n")
+    text.write(f"Winner: {winning_candidate}" + "\n")
+    text.write("-------------------------" + "\n")
